065b56634acb84e3b98183000729ee300d452ccd
Viktoriia <viculia.kravtsova@gmail.com>
2025-07-22 23:53:20 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace T_Rex
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            lblScore = new Label();
+            pictureBox1 = new PictureBox();
+            trex = new PictureBox();
+            obstacle1 = new PictureBox();
+            obstacle2 = new PictureBox();
+            obstacle3 = new PictureBox();
+            GameTimer = new System.Windows.Forms.Timer(components);
+            lblGameOver = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trex).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)obstacle1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)obstacle2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)obstacle3).BeginInit();
+            SuspendLayout();
+            // 
+            // lblScore
+            // 
+            lblScore.AutoSize = true;
+            lblScore.Location = new Point(28, 23);
+            lblScore.Name = "lblScore";
+            lblScore.Size = new Size(50, 20);
+            lblScore.TabIndex = 0;
+            lblScore.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Location = new Point(1, 383);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(860, 50);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // trex
+            // 
+            trex.BackColor = Color.Transparent;
+            trex.Image = Properties.Resources.dino_run;
+            trex.Location = new Point(190, 316);
+            trex.Name = "trex";
+            trex.Size = new Size(88, 92);
+            trex.SizeMode = PictureBoxSizeMode.AutoSize;
+            trex.TabIndex = 2;
+            trex.TabStop = false;
+            // 
+            // obstacle1
+            // 
+            obstacle1.BackColor = Color.Transparent;
+            obstacle1.Image = Properties.Resources.cactus1;
+            obstacle1.Location = new Point(351, 359);
+            obstacle1.Name = "obstacle1";
+            obstacle1.Size = new Size(37, 49);
+            obstacle1.SizeMode = PictureBoxSizeMode.StretchImage;
+            obstacle1.TabIndex = 3;
+            obstacle1.TabStop = false;
+            obstacle1.Tag = "obstacle";
+            // 
+            // obstacle2
+            // 
+            obstacle2.BackColor = Color.Transparent;
+            obstacle2.Image = Properties.Resources.cactus2;
+            obstacle2.Location = new Point(439, 359);
+            obstacle2.Name = "obstacle2";
+            obstacle2.Size = new Size(58, 49);
+            obstacle2.SizeMode = PictureBoxSizeMode.StretchImage;
+            obstacle2.TabIndex = 4;
+            obstacle2.TabStop = false;
+            obstacle2.Tag = "obstacle";
+            // 
+            // obstacle3
+            // 
+            obstacle3.BackColor = Color.Transparent;
+            obstacle3.Image = Properties.Resources.flying;
+            obstacle3.Location = new Point(284, 288);
+            obstacle3.Name = "obstacle3";
+            obstacle3.Size = new Size(57, 38);
+            obstacle3.SizeMode = PictureBoxSizeMode.StretchImage;
+            obstacle3.TabIndex = 5;
+            obstacle3.TabStop = false;
+            obstacle3.Tag = "obstacle";
+            // 
+            // GameTimer
+            // 
+            GameTimer.Enabled = true;
+            GameTimer.Interval = 15;
+            GameTimer.Tick += GameTimerEvent;
+            // 
+            // lblGameOver
+            // 
+            lblGameOver.AutoSize = true;
+            lblGameOver.Location = new Point(391, 147);
+            lblGameOver.Name = "lblGameOver";
+            lblGameOver.Size = new Size(50, 20);
+            lblGameOver.TabIndex = 6;
+            lblGameOver.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(862, 433);
+            Controls.Add(lblGameOver);
+            Controls.Add(obstacle3);
+            Controls.Add(obstacle2);
+            Controls.Add(obstacle1);
+            Controls.Add(trex);
+            Controls.Add(pictureBox1);
+            Controls.Add(lblScore);
+            Name = "Form1";
+            Tag = "obstacle";
+            Text = "T-Rex Endless  Runner";
+            Paint += FormPaintEvent;
+            KeyDown += KeyIsDown;
+            KeyUp += KeyIsUp;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trex).EndInit();
+            ((System.ComponentModel.ISupportInitialize)obstacle1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)obstacle2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)obstacle3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblScore;
+        private PictureBox pictureBox1;
+        private PictureBox trex;
+        private PictureBox obstacle1;
+        private PictureBox obstacle2;
+        private PictureBox obstacle3;
+        private System.Windows.Forms.Timer GameTimer;
+        private Label lblGameOver;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,262 @@
+﻿using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Media;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+
+namespace T_Rex
+{
+    public partial class Form1 : Form
+    {
+        Image backgroundImage;
+        Image backgroundImage2;
+
+        int backgroundHeight;
+        int backgroundWidth;
+        int bgPositionX = 0, bgPositionY = 0;
+        int bg2PositionX = 0;
+        int cactusSpeed = 12;
+        int formWidth;
+        int attackTimer = 0;
+        int attackR = 0;
+        int score = 0;
+        int speed = 10;
+        int[] flyingPosition = { 290, 363, 290, 363 };
+
+        bool jumping =  false;
+        bool changeAnim = false;
+        bool flyingAttack = false;
+        bool gameover = false;
+
+        Random random = new Random();
+
+        List<PictureBox> obstacles = new List<PictureBox>();
+        PictureBox hitBox = new PictureBox();
+        PrivateFontCollection newFont = new PrivateFontCollection();
+
+        SoundPlayer hitSound;
+        SoundPlayer plusOneSound;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            GameSetUp();
+            Reset();
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if(!gameover)
+            {
+                if(e.KeyCode == Keys.Up && !jumping)
+                {
+                    jumping = true;
+                }
+                if (e.KeyCode == Keys.Down && !jumping)
+                {
+
+                    if(changeAnim == false)
+                    {
+                        trex.Image = Properties.Resources.dino_crouch;
+                        changeAnim = true;
+                    }
+                    trex.Top = 352;
+                }
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down && !gameover)
+            {
+                trex.Image = Properties.Resources.dino_run;
+                trex.Top = 313;
+                changeAnim = false;
+            }
+            if(e.KeyCode == Keys.Enter && gameover)
+            {
+                Reset();
+            }
+        }
+
+        private void FormPaintEvent(object sender, PaintEventArgs e)
+        {
+            Graphics Canvas = e.Graphics;
+            Canvas.DrawImage(backgroundImage,bgPositionX,bgPositionY,backgroundWidth,backgroundHeight);
+            Canvas.DrawImage(backgroundImage2, bg2PositionX, bgPositionY, backgroundWidth, backgroundHeight);
+            Canvas.SmoothingMode = SmoothingMode.AntiAlias;
+            Canvas.InterpolationMode = InterpolationMode.HighQualityBicubic;
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            MoveBackgrounds();
+            this.Invalidate();
+
+            MoveObstacles   ();
+
+            lblScore.Text = "Score: " + score;
+
+            hitBox.Left = trex.Right - (hitBox.Width + 20);
+            hitBox.Top = trex.Top + 5;
+
+            if(jumping)
+            {
+                trex.Top -= speed;
+                if (trex.Top < 150)
+                {
+                    speed = -10;
+                }
+                if(trex.Top >312)
+                {
+                    jumping = false;
+                    trex.Top = 313;
+                    speed = 10;
+                }
+            }
+            foreach(PictureBox x in obstacles)
+            {
+               if(x.Bounds.IntersectsWith(hitBox.Bounds))
+                {
+                    GameTimer.Stop();
+                    trex.Image = Properties.Resources.dead;
+                    gameover = true;
+                    hitSound = new SoundPlayer(Properties.Resources.hit);
+                    lblGameOver.Visible = true;
+                    trex.Top = 313;
+                }
+
+            }    
+        }
+
+
+
+        private void GameSetUp()
+        {
+            backgroundWidth = 900;
+            backgroundHeight = 450;
+            bg2PositionX = 900; 
+            backgroundImage = Properties.Resources.bgPixels;
+            backgroundImage2 = Properties.Resources.bgPixels; //для бесшовной прокрутки фона
+            //Убирает мерцание при перерисовке (двойная буферизация)
+            this.DoubleBuffered = true; //включает отрисовку в скрытом буфере перед показом на экране
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true); //вручную задаёт стиль рендеринга формы, делая отрисовку ещё более плавной
+            formWidth = this.ClientSize.Width; //Сохраняет ширину клиентской области окна (без рамок и заголовков) для дальнейшего использования
+            this.BackColor = Color.Black;
+            obstacles.Add(obstacle1);
+            obstacles.Add(obstacle2);
+            obstacles.Add(obstacle3);
+
+            newFont.AddFontFile("font/pixel.ttf");
+            lblScore.Font = new Font(newFont.Families[0], 30, FontStyle.Bold);
+            lblScore.ForeColor = Color.White;
+            lblScore.BackColor = Color.Transparent;
+            lblScore.Text = "Score: 0";
+
+            lblGameOver = new Label();
+            lblGameOver.AutoSize = true;
+            lblGameOver.Font = new Font(newFont.Families[0], 48, FontStyle.Bold); 
+            lblGameOver.ForeColor = Color.Red;
+            lblGameOver.BackColor = Color.Transparent;
+            lblGameOver.Text = "GAME OVER";
+            lblGameOver.Visible = false;
+            // Центрируем надпись
+            lblGameOver.Left = (this.ClientSize.Width - lblGameOver.PreferredWidth) / 2;
+            lblGameOver.Top = (this.ClientSize.Height - lblGameOver.PreferredHeight) / 2;
+
+            this.Controls.Add(lblGameOver);
+            lblGameOver.BringToFront(); // на передний план
+
+            hitBox.BackColor = Color.Transparent;
+            hitBox.Width = trex.Width / 2;
+            hitBox.Height = trex.Height - 10;
+
+            this.Controls.Add(hitBox) ; //прямоугольника столкновения (hitbox), которым определяется, попал ли игрок в препятствие.
+       //     hitBox.BringToFront(); //на передний план
+            gameover = false;
+
+            attackR = random.Next(12, 20);
+        }
+
+        private void Reset() // перезагрузка
+        {
+            trex.Image = Properties.Resources.dino_run;
+            trex.Top = 313;
+            obstacle1.Left = formWidth + random.Next(100, 200);
+            obstacle2.Left = formWidth + random.Next(600, 800);
+            obstacle3.Left = formWidth + random.Next(200, 400);
+
+            GameTimer.Start();
+            lblGameOver.Visible = false;
+            score = 0;
+            attackTimer = 0;
+            speed = 10;
+            gameover = false;
+            changeAnim = false;
+            jumping = false;
+            cactusSpeed = 12;
+        }
+
+        private void MoveBackgrounds()
+        {
+            bgPositionX -= 1;
+            bg2PositionX -= 1;
+
+            if (bgPositionX < -backgroundWidth)
+            {
+                bgPositionX = bg2PositionX + backgroundWidth;
+            }
+            if (bg2PositionX < -backgroundWidth)
+            {
+                bg2PositionX = bgPositionX + backgroundWidth;
+            }
+        }
+
+        private void MoveObstacles() // препятствия
+        {
+            plusOneSound = new SoundPlayer(Properties.Resources.plusone);
+            if(!flyingAttack)
+            {
+                obstacle1.Left -= cactusSpeed;
+                obstacle2.Left -= cactusSpeed;
+            }
+            else
+            {
+                obstacle3.Left -= cactusSpeed;
+            }
+
+            if(attackTimer == attackR)
+            {
+                flyingAttack = true;
+                attackR = random.Next(12, 20);
+            }
+            if(attackTimer ==0)
+            {
+                flyingAttack = false;
+            }
+
+            if(obstacle1.Left < -100)
+            {
+                obstacle1.Left = obstacle2.Left + obstacle2.Width + formWidth + random.Next(100, 300);
+                attackTimer += 1;
+                score += 1;
+                plusOneSound.Play();
+            }
+            if (obstacle2.Left < -100)
+            {
+                obstacle2.Left = obstacle1.Left + obstacle1.Width + formWidth + random.Next(100, 300);
+                attackTimer += 1;
+                score += 1;
+                plusOneSound.Play();
+            }
+            if (obstacle3.Left < -100)
+            {
+                obstacle3.Left =  formWidth + random.Next(300, 400);
+                attackTimer -= 1;
+                score -= 1;
+                plusOneSound.Play();
+            }
+        }
+    }
+}
